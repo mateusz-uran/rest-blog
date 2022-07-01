2b65c1f9379cb7fd544e0f17c54dac97b98488ff
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-01 11:19:23 +0200

    Test if post exists in db

--- src/test/java/io/github/mateuszuran/restblog/service/PostServiceTest.java
+++ src/test/java/io/github/mateuszuran/restblog/service/PostServiceTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +30,7 @@ class PostServiceTest {
 
     @BeforeEach
     void initUseCase() {
+        service = new PostService(repository);
         post = Post.builder()
                 .id(1L)
                 .header("Title")
@@ -42,9 +43,22 @@ class PostServiceTest {
 
     @DisplayName("Test for saving post")
     @Test
-    void when_save_post_it_should_return_post() {
-        when(repository.save(post)).thenReturn(post);
+    void savedPost_success() {
+
+        when(repository.save(any(Post.class))).thenReturn(post);
+
         Post savedPost = repository.save(post);
         assertThat(savedPost.getHeader()).isNotNull();
+    }
+
+    @DisplayName("Test if saved post exists in db")
+    @Test
+    void post_exists_in_db() {
+        List<Post> postList = new ArrayList<>();
+        postList.add(post);
+        when(repository.findAll()).thenReturn(postList);
+
+        List<Post> fetchedPosts = service.getAllPosts();
+        assertThat(fetchedPosts.size()).isGreaterThan(0);
     }
 }
