8bb222e7f2de504f88e2278d45f2cc4b6fdbe1a9
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-04 14:34:36 +0200

    feat: about section

--- src/main/frontend/src/components/About.js
+++ src/main/frontend/src/components/About.js
@@ -8,12 +8,28 @@ const About = () => {
       <div className='wrapper'>
         <div className='leftSide'><img src={image} alt=''></img></div>
         <div className='rightSide'>
+          <div className='line'>&nbsp;</div>
           <div className='content'>
             <h2>About Me</h2>
-            <p>My name is Mateusz and I'm 25.
+            <p>My name is Mateusz and I'm 25. I've graduated at the beggining of 2022 IT studies
+              with specialization for web development.
             </p>
             <p>
-
+              <h4>Technologies and Languages I'm using</h4>
+              <ul>
+                <li>Java 11</li>
+                <li>Spring Boot</li>
+                <li>AWS
+                  <ul>
+                    <li>S3 Bucket</li>
+                    <li>Elastik Beanstalk</li>
+                    <li>Amplify</li>
+                  </ul>
+                </li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript - basics</li>
+              </ul>
             </p>
           </div>
         </div>
