7280a12d20d5a08e4962e78abb62a33fd10fdab6
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-04 17:50:40 +0200

    feat: frontend-add and delete image

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import About from './components/About';
+import AddPost from './components/AddPost';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Post from './components/Post';
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
+      <br/>
+      <AddPost />
       <br/>
       <Home />
       <br/>
--- src/main/frontend/src/components/Post.js
+++ src/main/frontend/src/components/Post.js
@@ -2,17 +2,29 @@ import React, { useState, useEffect, useCallback } from 'react';
 import '../App.css';
 import axios from 'axios'
 import {  useDropzone} from 'react-dropzone'
+import image from '../images/Basic_Element_15-30_(18).jpg'
+
+const client = axios.create({
+  baseURL: "http://localhost:8080/api/v1/post" 
+});
 
 const Post = () => {
 
   const [posts, setPosts] = useState([]);
 
-  const fetchPosts = () => {
-    axios.get("http://localhost:8080/api/v1/post").then(res => {
-      console.log(res);
-      setPosts(res.data);
-    });
+  const fetchPosts = async () => {
+    let response = await client.get();
+    setPosts(response.data)
   }
+
+  const deletePost = async (id) => {
+    await client.delete(`${id}`);
+    setPosts(
+       posts.filter((post) => {
+          return post.id !== id;
+       })
+    );
+ };
 
   useEffect(() => {
     fetchPosts();
@@ -22,12 +34,16 @@ const Post = () => {
 
     return (
       <div key={index}>
-        {post.id ? <img src={`http://localhost:8080/api/v1/post/${post.id}/download`} alt="" /> : null}
+        {post.id && post.imageName != null ? <img src={`http://localhost:8080/api/v1/post/${post.id}/download`} alt="" /> : <img src={image} alt=''></img>}
         <br/>
         <br/>
         <h2>{post.header}</h2>
         <p>{post.content}</p>
         <MyDropzone postId={post.id}/>
+        <br/>
+        <div className="button" >
+          <div className="delete-btn" onClick={() => deletePost(post.id)}>Delete</div>
+        </div>
         <br/>
       </div>
     )
