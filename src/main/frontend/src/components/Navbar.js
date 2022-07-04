f959a06e230c3bbfcb28033a7ce134af815c0793
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-04 11:08:10 +0200

    feat: responsive navbar

---
+++ src/main/frontend/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import '../App.css';
+import { FaBars } from "react-icons/fa";
+
+function Navbar() {
+
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <div className='Navbar'>
+      <div className='wrapper'>
+      <div className='leftSide'>
+        <div className='links' id={showLinks ? 'hidden' : ''}>
+          <a href='/home'>Home</a>
+          <a href='/about'>About</a>
+          <a href='/projects'>Projects</a>
+          <a href='/contact '>Contact</a>
+        </div>
+        <button onClick={() => setShowLinks(!showLinks)}><FaBars size='1.5em' fill='#007FFF'/></button>
+      </div>
+      <div className='rightSide'>
+          <a href='/login'><button>Login</button></a>
+          <a href='/register'><button>Register</button></a>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/main/frontend/src/components/Post.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import '../App.css';
+import axios from 'axios'
+import {  useDropzone} from 'react-dropzone'
+
+const Post = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    axios.get("http://localhost:8080/api/v1/post").then(res => {
+      console.log(res);
+      setPosts(res.data);
+    });
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return posts.map((post, index) => {
+
+    return (
+      <div key={index}>
+        {post.id ? <img src={`http://localhost:8080/api/v1/post/${post.id}/download`} alt="" /> : null}
+        <br/>
+        <br/>
+        <h1>{post.header}</h1>
+        <p>{post.content}</p>
+        <MyDropzone postId={post.id}/>
+        <br/>
+      </div>
+    )
+  })
+}
+
+function MyDropzone({postId}) {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    axios.post(
+      `http://localhost:8080/api/v1/post/${postId}/upload`,
+    formData, 
+    {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }
+    ).then(() => {
+      console.log("File uploaded successfully");
+      window.location.reload()
+    }).catch(err => {
+      console.log(err)
+    });
+
+  });
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
+
+export default Post
