3a22d30d15ee9392b4edf9b4210592b5eb78381f
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-06 17:41:56 +0200

    refacor: make one component for static content

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AddPostTest from './components/AddPostTest';
 import EditPost from './components/EditPost';
+import AddPost from './components/AddPost';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path={"/"} element={<Home />} />
-          <Route exact path={"/addpost"} element={<AddPostTest />} />
+          <Route exact path={"/addpost"} element={<AddPost />} />
           <Route exact path={"/editpost/:id"} element={<EditPost />} />
         </Routes>
       </Router>
