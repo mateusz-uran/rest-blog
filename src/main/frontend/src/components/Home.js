f99d2ea584714ccbd4bd8530aad06265890eb90d
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-07 09:45:10 +0200

    feat: list-comments

--- src/main/frontend/src/components/Home.js
+++ src/main/frontend/src/components/Home.js
@@ -5,6 +5,7 @@ import '../App.css';
 import image_main from '../images/undraw_hello_re_3evm.svg'
 import image_about from '../images/V1228_generated.jpg'
 import empty_image_post from '../images/Basic_Element_15-30_(18).jpg'
+import user_basic from '../images/Basic_Ui_(186).jpg'
 import { FaRegHandPeace } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
@@ -34,76 +35,94 @@ const Home = () => {
     fetchPosts();
   }, []);
 
-    return (
-      <div className='Home'>
-        <div className='manage'>
-          <Link className='default-link' to={"/addpost"}>Add Post</Link>
-        </div>
-        <div className='wrapper'>
-          <div className='main'>
-            <div className='leftSide'>
-              <p>Hello there <FaRegHandPeace fill='#007FFF' />, I'm</p>
-              <h1>Mateusz Uranowski</h1>
-              <p>and I'm a web developer.</p>
-            </div>
-            <div className='rightSide'>
-              <img src={image_main} alt=''></img>
-            </div>
+  return (
+    <div className='Home'>
+      <div className='manage'>
+        <Link className='default-link' to={"/addpost"}>Add Post</Link>
+      </div>
+      <div className='wrapper'>
+        <div className='main'>
+          <div className='leftSide'>
+            <p>Hello there <FaRegHandPeace fill='#007FFF' />, I'm</p>
+            <h1>Mateusz Uranowski</h1>
+            <p>and I'm a web developer.</p>
+          </div>
+          <div className='rightSide'>
+            <img src={image_main} alt=''></img>
           </div>
         </div>
-        <div className='wrapper'>
-          <div className='about'>
-            <div className='leftSide'><img src={image_about} alt=''></img></div>
-            <div className='rightSide'>
-              <div className='line'>&nbsp;</div>
-              <div className='content'>
-                <h2>About Me</h2>
-                <p>My name is Mateusz and I'm 25. I've graduated at the beggining of 2022 IT studies
-                  with specialization for web development.
-                </p>
-                <div>
-                  <h4>Technologies and Languages I'm using</h4>
-                  <ul>
-                    <li>Java 11</li>
-                    <li>Spring Boot</li>
-                    <li>AWS
-                      <ul>
-                        <li>S3 Bucket</li>
-                        <li>Elastik Beanstalk</li>
-                        <li>Amplify</li>
-                      </ul>
-                    </li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript - basics</li>
-                  </ul>
-                </div>
+      </div>
+      <div className='wrapper'>
+        <div className='about'>
+          <div className='leftSide'><img src={image_about} alt=''></img></div>
+          <div className='rightSide'>
+            <div className='line'>&nbsp;</div>
+            <div className='content'>
+              <h2>About Me</h2>
+              <p>My name is Mateusz and I'm 25. I've graduated at the beggining of 2022 IT studies
+                with specialization for web development.
+              </p>
+              <div>
+                <h4>Technologies and Languages I'm using</h4>
+                <ul>
+                  <li>Java 11</li>
+                  <li>Spring Boot</li>
+                  <li>AWS
+                    <ul>
+                      <li>S3 Bucket</li>
+                      <li>Elastik Beanstalk</li>
+                      <li>Amplify</li>
+                    </ul>
+                  </li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript - basics</li>
+                </ul>
               </div>
             </div>
           </div>
         </div>
-        <div className='wrapper'>
-          <div className='projects'>
-            {
-              posts.map((post, index) => (
-                <div className='postContainer' key={index}>
-                  <div className='text'>
-                    <h3>{post.header}</h3>
-                    <p>{post.content}</p>
-                  </div>
-                  <div className='image'>
-                    {post.id && post.imageName != null ? <img src={`http://localhost:8080/api/v1/post/${post.id}/download`} alt="" /> : <img src={empty_image_post} alt=''></img>}
-                    <MyDropzone postId={post.id} />
-                    <div className="delete-btn" onClick={() => deletePost(post.id)}>Delete</div>
-                    <Link to={`/editpost/${post.id}`}>Edit</Link>
-                  </div>
+      </div>
+      <div className='wrapper'>
+        <div className='projects'>
+          {
+            posts.map((post, index) => (
+              <div className='postContainer' key={index}>
+                <div className='text'>
+                  <h3>{post.header}</h3>
+                  <p>{post.content}</p>
                 </div>
-              ))
-            }
-          </div>
+                <div className='image'>
+                  {post.id && post.imageName != null ? <img src={`http://localhost:8080/api/v1/post/${post.id}/download`} alt="" /> : <img src={empty_image_post} alt=''></img>}
+                  <MyDropzone postId={post.id} />
+                  <div className="delete-btn" onClick={() => deletePost(post.id)}>Delete</div>
+                  <Link to={`/editpost/${post.id}`}>Edit</Link>
+                </div>
+                {
+                  post.comments.map((comment, index) => (
+                    <div className='comments' key={index}>
+                      <div className='commentContainer'>
+                        <div className='leftSide'>
+                          <div>
+                            <img src={user_basic} alt=''></img>
+                          </div>
+                          <div>{comment.date}</div>
+                        </div>
+                        <div className='rightSide'>
+                          <div>{comment.author}</div>
+                          <div>{comment.content}</div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            ))
+          }
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 function MyDropzone({ postId }) {
