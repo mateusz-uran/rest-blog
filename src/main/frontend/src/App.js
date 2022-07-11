934c4242076462d454102eb50382ea5293cb6159
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-11 10:36:37 +0200

    feat: footer with links

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path={"/"} element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
