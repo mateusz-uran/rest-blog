6ffd9daac347a399ab295d67ef315d23098f4449
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-12 11:47:58 +0200

    refactor: clean code

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,14 +6,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer';
 import Main from './components/Main';
 import About from './components/About';
-import WYSIWYG from './components/WYSIWYG';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <WYSIWYG />
         <Main/>
         <About />
         <Home/>
--- src/main/frontend/src/components/WYSIWYG.js
+++ src/main/frontend/src/components/WYSIWYG.js
@@ -3,36 +3,11 @@ import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import '../App.css';
-import axios from 'axios';
-import Moment from 'moment';
 
 const WYSIWYG = ({ postId }) => {
   const [editorState, setEditorState] = React.useState(
     () => EditorState.createEmpty(),
   );
-
-  const [comment, setComment] = useState({
-    content: "",
-    date: "",
-    author: ""
-  });
-
-  const { content } = comment;
-
-  const onInputChange = (e) => {
-    setComment({ ...comment, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const formatDate = Moment().format('DD-MM-YYYY, h:mm A')
-    const defaultUser = "user";
-    comment.date = formatDate;
-    comment.author = defaultUser;
-    await axios.post(`http://localhost:8080/api/v1/post/${postId}/add-comment`, comment);
-    setComment('');
-    e.target.reset();
-  };
 
   return (
     <div className="editor">
@@ -43,8 +18,6 @@ const WYSIWYG = ({ postId }) => {
           wrapperClassName="wrapperClassName"
           editorClassName="editorClassName"
           onEditorStateChange={setEditorState}
-          defaultValue={content || ''}
-          onChange={(e) => onInputChange(e)}
         />
       </div>
     </div>
