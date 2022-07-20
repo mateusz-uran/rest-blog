b3798d849e08a3d8c34dde1982c4a4109c374e2c
mateusz-uran <mateusz.uranowski@onet.pl>
2022-07-20 16:45:27 +0200

    refactor: get ready for new funcionalities

--- src/main/java/io/github/mateuszuran/restblog/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.mateuszuran.restblog.repository;
-
-import io.github.mateuszuran.restblog.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/io/github/mateuszuran/restblog/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.mateuszuran.restblog.repository;
-
-import io.github.mateuszuran.restblog.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
