8a0309665473d44fa651b62aa5704839deb03d88
mateusz-uran <mateusz.uranowski@onet.pl>
2022-09-05 19:01:26 +0200

    style: animate about section

--- src/main/frontend/src/components/About.js
+++ src/main/frontend/src/components/About.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import image_about from '../images/V1228_generated.jpg';
 import '../App.css';
 import PDF from '../documents/CV-pdf.pdf';
 import { AiOutlineFilePdf } from 'react-icons/ai';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
 const About = () => {
+
+  useEffect(() => {
+    AOS.init({duration: 1000, once: true});
+  }, []);
+
   return (
     <div className='wrapper'>
       <div id='about' className='about'>
         <div className='leftSide'>
-          <div className='img-bng'>
-          </div>
+          <div data-aos='fade-up' className='img-bng'></div>
           <img src={image_about} alt=''></img>
         </div>
         <div className='rightSide'>
-          <div className='line'>&nbsp;</div>
           <div className='content'>
             <h2>About Me</h2>
             <p>My name is Mateusz and I'm 25. I've graduated at the beggining of 2022 IT studies
               with specialization for web development.
             </p>
-            <div>
-              <h4>Technologies and Languages I'm using</h4>
-              <ul>
+            <h4>Technologies and Languages I'm using</h4>
+            <div className='list'>
+              <ul className='tech-list'>
                 <li>Java 11</li>
                 <li>Spring Boot</li>
                 <li>AWS
@@ -36,6 +43,48 @@ const About = () => {
                 <li>React</li>
                 <li>HTML</li>
                 <li>CSS</li>
+              </ul>
+              <ul className='bar-list'>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='java-11-bar-progress'>&nbsp;</div>
+                  </div>
+                </li>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='spring-boot-bar-progress'>&nbsp;</div>
+                  </div>
+                </li>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='aws-bar-progress'>&nbsp;</div>
+                  </div>
+                  <ul>
+                    <li>&nbsp;</li>
+                    <li>&nbsp;</li>
+                    <li>&nbsp;</li>
+                  </ul>
+                </li>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='js-progress'>&nbsp;</div>
+                  </div>
+                </li>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='react-progress'>&nbsp;</div>
+                  </div>
+                </li>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='html-progress'>&nbsp;</div>
+                  </div>
+                </li>
+                <li>
+                  <div className='bng-bar'>
+                    <div data-aos='fade-right' className='css-progress'>&nbsp;</div>
+                  </div>
+                </li>
               </ul>
             </div>
             <div className='pdf-link'>
