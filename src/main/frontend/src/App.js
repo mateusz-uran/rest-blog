79c8104ee9df81f6eca27ca3c691e96bf34f6682
mateusz-uran <mateusz.uranowski@onet.pl>
2022-09-06 14:15:04 +0200

    style: add hero effect and fix scroll animation

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
@@ -8,15 +8,40 @@ import Main from './components/Main';
 import About from './components/About';
 
 function App() {
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+
+    let progressBarHandler = () => {
+
+      const totalScroll = document.documentElement.scrollTop;
+      const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scroll = `${totalScroll / windowHeight}`;
+
+      setScroll(scroll);
+    }
+
+    window.addEventListener("scroll", progressBarHandler);
+
+    return () => window.removeEventListener("scroll", progressBarHandler);
+  }, []);
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Main />
-        <About />
-        <Home />
-        <Footer />
-      </Router>
+      <div className='hero-effect'>
+        <div className='overlay'>
+          <div id="progressBarContainer">
+            <div id="progressBar" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 1.5}` }} />
+          </div>
+          <Router>
+            <Navbar />
+            <Main />
+            <About />
+            <Home />
+            <Footer />
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
--- src/main/frontend/src/components/Main.js
+++ src/main/frontend/src/components/Main.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaRegHandPeace } from "react-icons/fa";
 import image_main from '../images/4002785.jpg';
 import '../App.css';
-import { duration } from 'moment/moment';
+
 
 const Main = () => {
   const [scroll, setScroll] = useState(0);
@@ -29,9 +29,9 @@ const Main = () => {
           <p>Hello there <FaRegHandPeace fill='#007FFF' />, my name is</p>
           <h1>Mateusz Uranowski</h1>
           <p>and I'm a web developer.
-            <span id="show" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 8}`}} >I guess</span>
-            <span id="show" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 4}`}} >Some day</span>
-            <span id="show" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 2}`}} >Finally</span>
+            <span id="show" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 8}` }} >I guess</span>
+            <span id="show" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 4}` }} >Some day</span>
+            <span id="show" style={{ transform: `scale(${scroll}, 1)`, opacity: `${scroll * 2}` }} >Finally</span>
           </p>
         </div>
         <div className='rightSide'>
