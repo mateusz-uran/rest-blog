843c3f7a3a43e7c0a760ba357a9a96900174ffe1
mateusz-uran <mateusz.uranowski@onet.pl>
2022-09-09 14:53:16 +0200

    refactor: root directory

--- backend/src/main/java/io/github/mateuszuran/restblog/config/AmazonConfig.java
+++ backend/src/main/java/io/github/mateuszuran/restblog/config/AmazonConfig.java
@@ -14,8 +14,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials credentials = new BasicAWSCredentials(
-                "AKIATBKNJJHD6ORVPCA6",
-                "N9CJ6uCkszGimE0+E/l3cU3xhwKcvkPsjfKmMPwZ"
+                "exposed",
+                "exposed"
         );
         return AmazonS3ClientBuilder
                 .standard()
